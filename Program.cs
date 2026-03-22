cffbd7674623d42ba7b14d0c8c5b68df37aa742c
NULL-X9C <reallygoat69@gmail.com>
2026-03-22 03:44:51 +0300

    дз линку

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using DotaParser52.Homeworks._14._03;
 using DotaParser52.ClassWork;
 
+using  DotaParser52.Homeworks._21._03;
+
 // var FiltProc = new DotaParserProcessor();
 //
 // var sensorProc = new SensorProcessor();
--- obj/Debug/net8.0/DotaParser52.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotaParser52.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotaParser52")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702dc462d4a2de917178c35c44a37b06f7f6610b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded5facc1b44a30319b653206fda74f1d581817c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotaParser52")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotaParser52")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
