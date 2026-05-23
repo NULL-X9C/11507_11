d12636834806f01cc1c10ebbb49a23e725c961a2
NULL-X9C <reallygoat69@gmail.com>
2026-05-23 17:59:29 +0300

    kr

---
+++ 11507/ControlWorks/CW2/CwSolves2/Problem2.cs
@@ -0,0 +1,48 @@
+namespace CwSolves2;
+using System.Threading.Tasks;
+
+// TODO: Напишите TaskDispatcher: 
+//создайте список из 50 задач (Action). Запустите их так, чтобы в любой момент времени выполнялось не более 5 задач 
+//  (используйте обычный Semaphore(5, 5)).
+// fill
+//process
+
+public class MyTaskDispatcher
+{
+    private readonly Semaphore Sem = new Semaphore(5, 5);
+    private readonly List<Action> Tasks = new List<Action>();
+
+    public void Fill()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            int taskNumber = i;
+            Tasks.Add(() =>
+            {
+                Thread.Sleep(100);
+            });
+        }
+    }
+
+    public void Run()
+    {
+        var taskArray = new Task[50];
+        for (int i = 0; i < 50; i++)
+        {
+            Action action = Tasks[i];
+            taskArray[i] = Task.Run(() =>
+            {
+                Sem.WaitOne();
+                try
+                {
+                    action();
+                }
+                finally
+                {
+                    Sem.Release();
+                }
+            });
+        }
+        Task.WaitAll(taskArray);
+    }
+}
