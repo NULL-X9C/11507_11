cfffe33d4c5b28b29244a64d868a73dcb107a54e
NULL-X9C <reallygoat69@gmail.com>
2026-05-23 18:03:59 +0300

    kr

--- 11507/ControlWorks/CW2/CwSolves2/Problem2.cs
+++ 11507/ControlWorks/CW2/CwSolves2/Problem2.cs
@@ -16,7 +16,6 @@ public class MyTaskDispatcher
     {
         for (int i = 0; i < 50; i++)
         {
-            int taskNumber = i;
             Tasks.Add(() =>
             {
                 Thread.Sleep(100);
